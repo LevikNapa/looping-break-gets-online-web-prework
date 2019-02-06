10bb9d7806862a5e87ef79c3c3671015819e8cbe
Levik Naparstek <levik010@gmail.com>
2019-02-06 17:40:54 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -4,8 +4,8 @@ def levitation_quiz
 	  puts "What is the spell that enacts levitation?"
 	  answer == gets.chomp
 	  break if 
-	  answer == ""
-	  
+	  answer == "Wingardium Leviosa"
+	 end 
 end
 
 
